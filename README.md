a138797c56a77903807250c7b8edb51694505283
unknown <alexw@pc5-tea.hsps.tp.edu.tw>
2016-09-05 14:53:02 +0800

    modified by alexw

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # w10_rmapp
+
 移除 windows 10 內建的 App
+
+Fork from [W4RH4WK/Debloat-Windows-10](https://github.com/W4RH4WK/Debloat-Windows-10)
+
+# 使用方法
+
+開始 / Powershell 右鍵 管理員開啟
+
+切換到本腳本資料夾(如 c:\Users\kerker\desktop\w10_rmapp\)
+
+    PS> cd c:\Users\kerker\desktop\w10_rmapp\
+
+關閉安全限制
+
+    PS> Set-ExecutionPolicy Unrestricted
+
+選 Y
+
+執行欲執行的腳本
+
+移除 內建 APP
+
+    PS> w10_rmapp.ps1
+
+移除 OneDrive
+
+    PS> remove-onedrive.ps1
+
+
+## License
+
+    "THE BEER-WARE LICENSE" (Revision 42):
+
+    As long as you retain this notice you can do whatever you want with this
+    stuff. If we meet some day, and you think this stuff is worth it, you can
+    buy us a beer in return.
+
+    This project is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.
---
+++ scripts/w10_rmapp.ps1
@@ -0,0 +1,64 @@
+# Requires -RunAsAdministrator
+# 移除 CloudContent Consumer Experience 避免 被安裝(可由群組原則處理)
+REG ADD "HKLM\Software\Policies\Microsoft\Windows\CloudContent" /v DisableWindowsConsumerFeatures /d 1 /t REG_DWORD /f
+
+echo "移除內建APP"
+# 以下可以自由選用 加 # 代表不移除
+$apps = @(
+    # default Windows 10 apps
+    "Microsoft.3DBuilder"
+    "Microsoft.Appconnector"
+    "Microsoft.BingFinance"
+    "Microsoft.BingNews"
+    "Microsoft.BingSports"
+    "Microsoft.BingWeather"
+    "Microsoft.FreshPaint"
+    "Microsoft.Getstarted"
+    "Microsoft.MicrosoftOfficeHub"
+    "Microsoft.MicrosoftSolitaireCollection"
+    "Microsoft.MicrosoftStickyNotes"
+    "Microsoft.Office.OneNote"
+    "Microsoft.OneConnect"
+    "Microsoft.People"
+    "Microsoft.SkypeApp"
+    #"Microsoft.Windows.Photos"
+    #"Microsoft.WindowsAlarms"
+    #"Microsoft.WindowsCalculator"
+    #"Microsoft.WindowsCamera"
+    "Microsoft.WindowsMaps"
+    "Microsoft.WindowsPhone"
+    #"Microsoft.WindowsSoundRecorder"
+    "Microsoft.WindowsStore"
+    "Microsoft.XboxApp"
+    "Microsoft.ZuneMusic"
+    "Microsoft.ZuneVideo"
+    "microsoft.windowscommunicationsapps"
+    "Microsoft.MinecraftUWP"
+    "Microsoft.CommsPhone"
+    "Microsoft.ConnectivityStore"
+    "Microsoft.Messaging"
+    "Microsoft.Office.Sway"
+
+    # non-Microsoft
+    "*Twitter*"
+    "*Flipboard*"
+    "*Shazam*"
+    "king.com.*"
+    "*iHeartRadio*"
+    "*network*"
+    "*Eclipse ma*"
+    "*Wunderlist*"
+    "*translat*"
+    "*Duolingo*"
+    "*Photoshop*"
+    "*Pics*"
+)
+
+foreach ($app in $apps) {
+    echo "Trying to remove $app"
+
+    Get-AppxPackage -Name $app -AllUsers | Remove-AppxPackage
+    Get-AppXProvisionedPackage -Online |
+        where DisplayName -EQ $app |
+        Remove-AppxProvisionedPackage -Online
+}
